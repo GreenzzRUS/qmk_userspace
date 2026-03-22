d2cc25f387cccd8440c6f0d47a97625dd38fa284
GreenzzRUS <113213874+GreenzzRUS@users.noreply.github.com>
2026-03-23 00:04:16 +0500

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vendor/keymap.c
@@ -22,6 +22,21 @@
 
 #define RESET QK_BOOT
 
+enum charybdis_keymap_layers {
+    LAYER_BASE = 0,
+    LAYER_LOWER,
+    LAYER_RAISE,
+    LAYER_POINTER,
+    LAYER_DEF,
+    LAYER_GAM,
+    LAYER_GFN,
+    LAYER_SYM,
+    LAYER_NAV,
+    LAYER_NUM,
+    LAYER_ALT,
+    LAYER_CMD,
+};
+
 #define ___ KC_NO
 
 #define LA_SYM MO(_SYM)
@@ -45,24 +60,12 @@
 #define TAB_L C(S(KC_TAB))
 #define TAB_R C(KC_TAB)
 
-enum layers {
-    _DEF,
-    _GAM,
-    _GFN,
-    _SYM,
-    _NAV,
-    _NUM,
-    _ALT,
-    _CMD,
-    LAYER_POINTER,
-};
 enum keycodes {
     // Custom oneshot mod implementation with no timers.
     OS_SHFT = SAFE_RANGE,
     OS_CTRL,
     OS_ALT,
     OS_CMD,
-
     SW_WIN,  // Switch to next window         (alt-tab)
     SW_TAB,  // Switch to next browser tab    (ctrl-tab)
 };
@@ -89,28 +92,28 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_DEF] = LAYOUT(
+    [LAYER_DEF] = LAYOUT(
 KC_NO,      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,        KC_NO,      
 KC_NO,      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,             KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,     KC_NO,      
 KC_NO,      KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,     KC_NO,      
                                     KC_CAPS,    KC_SPC,     LA_NAV,           LA_SYM,     KC_LSFT
     ),
 
-    [_GAM] = LAYOUT(
+    [LAYER_GAM] = LAYOUT(
 KC_NO,     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,             ___,        ___,        KC_UP,      ___,        KC_TAB,    KC_NO,     
 KC_NO,     KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,             ___,        KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_RSFT,   KC_NO,     
 KC_NO,     KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,             ___,        ___,        ___,        ___,        KC_RCTL,   KC_NO,     
                                    KC_LALT,    KC_SPC,     LA_GFN,           DF(_DEF),   KC_SPC
     ),
 
-    [_GFN] = LAYOUT(
+    [LAYER_GFN] = LAYOUT(
 KC_NO,     KC_5,       KC_1,       KC_2,       KC_3,       KC_4,             KC_NUM,     ___,        ___,        ___,        ___,        KC_NO,     
 KC_NO,     KC_0,       KC_6,       KC_7,       KC_8,       KC_9,             ___,        ___,        ___,        ___,        ___,        KC_NO,     
 KC_NO,     KC_G,       KC_J,       KC_I,       KC_M,       KC_T,             ___,        ___,        ___,        QK_RBT,     QK_BOOT,    KC_NO,     
                                    KC_ESC,     KC_ENT,     _______,          _______,    _______
     ),
 
-    [_SYM] = LAYOUT(
+    [LAYER_SYM] = LAYOUT(
 KC_NO,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,             KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_NO,     
 KC_NO,     KC_SCLN,    GRV,        KC_GRV,     QUOT,       KC_MINS,          KC_PLUS,    OS_SHFT,    OS_CTRL,    OS_ALT,     OS_CMD,     KC_NO,     
 KC_NO,     BSL,        DPIPE,      SCLN,       KC_LBRC,    KC_UNDS,          KC_EQL,     KC_RBRC,    CLN,        PIPE,       KC_BSLS,    KC_NO,     
@@ -118,28 +121,28 @@ KC_NO,     BSL,        DPIPE,      SCLN,       KC_LBRC,    KC_UNDS,          KC_
     ),
 
 
-    [_NAV] = LAYOUT(
+    [LAYER_NAV] = LAYOUT(
 KC_NO,     SW_TAB,     SW_WIN,     TAB_L,      TAB_R,      KC_ESC,           KC_ESC,     KC_HOME,    KC_END,     KC_BSPC,    KC_DEL,    KC_NO,     
 KC_NO,     OS_CMD,     OS_ALT,     OS_CTRL,    OS_SHFT,    KC_ENT,           KC_ENT,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,   KC_NO,     
 KC_NO,     SPACE_L,    SPACE_R,    DF(_GAM),   KC_PSCR,    KC_TAB,           KC_TAB,     KC_PGUP,    KC_PGDN,    KC_QUOT,    KC_NUM,    KC_NO,     
                                    LA_CMD,     _______,    _______,          _______,    _______
     ),
 
-    [_NUM] = LAYOUT(
+    [LAYER_NUM] = LAYOUT(
 KC_NO,     KC_P1,      KC_P2,      KC_P3,      KC_P4,      KC_P5,            KC_P6,      KC_P7,      KC_P8,      KC_P9,      KC_P0,     KC_NO,     
 KC_NO,     OS_CMD,     OS_ALT,     OS_CTRL,    OS_SHFT,    KC_F11,           KC_F12,     OS_SHFT,    OS_CTRL,    OS_ALT,     OS_CMD,    KC_NO,     
 KC_NO,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,            KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_NO,     
                                    _______,    _______,    _______,          _______,    _______
     ),
 
-    [_ALT] = LAYOUT(
+    [LAYER_ALT] = LAYOUT(
 KC_NO,     RALT(KC_Q), RALT(KC_W), RALT(KC_E), RALT(KC_R), RALT(KC_T),       RALT(KC_Y), RALT(KC_U), RALT(KC_I),    RALT(KC_O),   RALT(KC_P),       KC_NO,     
 KC_NO,     RALT(KC_A), RALT(KC_S), RALT(KC_D), RALT(KC_F), RALT(KC_G),       RALT(KC_H), RALT(KC_J), RALT(KC_K),    RALT(KC_L),   RALT(KC_SCLN),    KC_NO,     
 KC_NO,     RALT(KC_Z), RALT(KC_X), RALT(KC_C), RALT(KC_V), RALT(KC_B),       RALT(KC_N), RALT(KC_M), RALT(KC_COMM), RALT(KC_DOT), RALT(KC_SLSH),    KC_NO,     
                                    _______,    NBSP,       KC_LSFT,          _______,    _______
     ),
 
-    [_CMD] = LAYOUT(
+    [LAYER_CMD] = LAYOUT(
 KC_NO,     MEH(KC_Q),  MEH(KC_W),  MEH(KC_E),  MEH(KC_R),  MEH(KC_T),        MEH(KC_Y),  KC_VOLD,    KC_MUTE,       KC_VOLU,     MEH(KC_P),       KC_NO,     
 KC_NO,     MEH(KC_A),  MEH(KC_S),  MEH(KC_D),  MEH(KC_F),  MEH(KC_G),        MEH(KC_H),  KC_MPRV,    KC_MPLY,       KC_MNXT,     MEH(KC_SCLN),    KC_NO,     
 KC_NO,     MEH(KC_Z),  MEH(KC_X),  MEH(KC_C),  MEH(KC_V),  MEH(KC_B),        MEH(KC_N),  MEH(KC_M),  MEH(KC_COMM),  MEH(KC_DOT), MEH(KC_SLSH),    KC_NO,     
@@ -240,14 +243,17 @@ void matrix_scan_user(void) {
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Логика три-слоя
-    state = update_tri_layer_state(state, _SYM, _NAV, _NUM);
-    
+    layer_state_t layer_state_set_user(layer_state_t state) {
     // Логика снипинга
     #ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
     charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
     #endif
+
+    // Логика из keymap-36.c для слоя NUM
+    return update_tri_layer_state(state, LAYER_SYM, LAYER_NAV, LAYER_NUM);
+}
+    
+
     
     return state;
 }
